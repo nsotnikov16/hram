4713862231aa627e52abba984085d44ad6b0e998
Nikita <nsotnikov16@yandex.ru>
2021-12-06 17:39:41 +0500

    update

--- hram/js/script.js
+++ hram/js/script.js
@@ -10,21 +10,38 @@ var swiper = new Swiper(".gallery", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    481: {
+      slidesPerView: 2,
+    },
+    600: {
+      slidesPerView: 3,
+    },
+    769: {
+      slidesPerView: 4,
+    },
+    1180: {
+      slidesPerView: 6,
+    },
+  },
 });
 
 //Scroll
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
+  smoothLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute("href");
 
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
     });
-};
+  });
+}
 
 /* Попапы */
 const popups = document.querySelectorAll(".popup");
@@ -33,7 +50,6 @@ function openPopup(pointer, target) {
   popup.classList.add("popup_opened");
   if (pointer === "photo") {
     popup.querySelector(".popup__image").src = target.src;
-    
   }
 
   document.addEventListener("keydown", handleEscClose);
@@ -76,3 +92,18 @@ if (popups.length > 0) {
     popupContainer.addEventListener("click", (evt) => handleOverlayClick(evt));
   });
 }
+
+// Меню
+const menuBtn = document.querySelector(".nav__button");
+const navigation = document.querySelector(".nav");
+menuBtn.addEventListener("click", () => {
+  navigation.classList.contains("nav_open")
+    ? navigation.classList.remove("nav_open")
+    : navigation.classList.add("nav_open");
+});
+
+/* document.addEventListener("click", ({ target }) => {
+  
+  if (navigation.classList.contains("nav_open") && target !== document.querySelector('.menu'))
+    navigation.classList.remove("nav_open");
+}); */
